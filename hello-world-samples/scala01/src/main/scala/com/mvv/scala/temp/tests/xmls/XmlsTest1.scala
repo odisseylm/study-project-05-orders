5326d2f4c8e73f053fc98dd2efb9d2a0489f0905
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-01-18 04:10:20 +0200

    Scala ... => other scala features

--- hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/xmls/XmlsTest1.scala
+++ hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/xmls/XmlsTest1.scala
@@ -1,5 +1,11 @@
 package com.mvv.scala.temp.tests.xmls
 
+import scala.compiletime.uninitialized
+
+
+def emptyElem: scala.xml.Elem =
+  scala.xml.Elem("p", "l", scala.xml.Null, scala.xml.NamespaceBinding("p", "https://www.www.www", scala.xml.TopScope), false)
+
 
 @main
 def aa(): Unit = {
@@ -27,3 +33,39 @@ def aa(): Unit = {
   println(star.getClass)
   println(star)
 }
+
+@main
+def aa2(): Unit = {
+
+  //var star: scala.xml.Elem = uninitialized
+  var star: scala.xml.Elem = emptyElem
+  {
+    import scala.language.unsafeNulls // << ============== VERY important
+    star = <star><title>Sun</title></star>
+  }
+
+  println(star.getClass)
+  println(star)
+}
+
+@main
+def aa3(): Unit = {
+  val star = {
+    import scala.language.unsafeNulls // << ============== VERY important
+    <star><title>Sun</title></star>
+  }
+
+  println(star.getClass)
+  println(star)
+}
+
+/*
+@main
+def aa4(): Unit = {
+  val star: scala.xml.Elem = <star><title>Sun</title></star>.nn       // Not compiled
+  val star2: scala.xml.Elem = ( <star><title>Sun</title></star> ).nn  // Not compiled
+
+  println(star.getClass)
+  println(star)
+}
+*/
