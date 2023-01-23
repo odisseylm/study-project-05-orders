bd1b2955420deca9e9d33c81418d1661603a200b
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-01-23 20:58:28 +0200

    Scala ... => other scala features => Working macros!!!

--- hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/macros2/macrosProdDependencies.scala
+++ hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/macros2/macrosProdDependencies.scala
@@ -3,19 +3,25 @@ package com.mvv.scala.macros
 import scala.annotation.nowarn
 
 
-// currently owner will ne Any (probably later I'll manage to set it properly)
-class PropValue[PropertyType, OwnerType] protected (
+trait PropValue[PropertyType, OwnerType] :
+ def propName: String
+ def value: Option[PropertyType]
+  // currently owner will be Any (probably later I'll find how to fill it properly)
+  def propOwner: Option[Class[OwnerType]]
+
+
+private class PropValueImpl[PropertyType, OwnerType] (
   val propName: String,
   val value: Option[PropertyType],
   val propOwner: Option[Class[OwnerType]],
-)
+) extends PropValue[PropertyType, OwnerType]
 
 
 object PropValue :
   def apply[PropertyType, OwnerType](propName: String, propValue: PropertyType|Null, propOwner: Class[OwnerType]|Null): PropValue[PropertyType, OwnerType] =
-    new PropValue(propName, toOption(propValue), toOption(propOwner))
+    new PropValueImpl(propName, toOption(propValue), toOption(propOwner))
   def apply[PropertyType, OwnerType](propName: String, propValue: Option[PropertyType], propOwner: Class[OwnerType]|Null): PropValue[PropertyType, OwnerType] =
-    new PropValue(propName, toOption(propValue), toOption(propOwner))
+    new PropValueImpl(propName, toOption(propValue), toOption(propOwner))
   // !!! Do not use default param with macros (it generates really bad code with temporary variables for defaults)
   def apply[PropertyType, OwnerType](propName: String, propValue: PropertyType|Null): PropValue[PropertyType, OwnerType] =
     apply[PropertyType, OwnerType](propName, propValue, null)
@@ -27,3 +33,18 @@ object PropValue :
 //noinspection TypeCheckCanBeMatch
 private def toOption[T](@nowarn v: Any): Option[T] =
   if v.isInstanceOf[Option[T]] then v.asInstanceOf[Option[T]] else Option[T](v.asInstanceOf[T])
+
+
+
+
+/*
+// currently owner will be Any (probably later I'll manage to set it properly)
+class ReadonlyProp[PropertyType, OwnerType] protected (
+  val propName: String,
+  def value: ()=>PropertyType,
+  val propOwner: Option[Class[OwnerType]],
+  )
+*/
+
+
+
