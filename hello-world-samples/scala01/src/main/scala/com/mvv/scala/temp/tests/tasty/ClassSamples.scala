e71fbc58931505a32800ee799fe358132ffcbb3e
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-02-03 03:59:36 +0200

    Scala ... => other scala features => using tasty for generating bean-info => Initial

--- hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/tasty/ClassSamples.scala
+++ hello-world-samples/scala01/src/main/scala/com/mvv/scala/temp/tests/tasty/ClassSamples.scala
@@ -73,12 +73,14 @@ end UsualScalaClassSample
 trait Trait1 :
   val trait1Val: String = ""
   var trait1Var: String = ""
-  def trait1Method: String = ""
+  def trait1ValMethod: String = ""
+  def trait1Method(): String = ""
 
 trait Trait2 :
   val trait2Val: String = ""
   var trait2Var: String = ""
-  def trait2Method: String = ""
+  def trait2ValMethod: String = ""
+  def trait2Method(): String = ""
 
 class BaseClass1 :
   private val privateValField0: String = ""
@@ -89,9 +91,9 @@ class BaseClass1 :
   protected var protectedVarField0: String = ""
   var publicVarField0: String = ""
 
-  private def privateMethod0: String = ""
-  protected def protectedMethod0: String = ""
-  def publicMethod0: String = ""
+  private def privateValMethod0: String = ""
+  protected def protectedValMethod0: String = ""
+  def publicValMethod0: String = ""
 
 
   private val privateValField1: String = ""
@@ -102,9 +104,9 @@ class BaseClass1 :
   protected var protectedVarField1: String = ""
   var publicVarField1: String = ""
 
-  private def privateMethod1: String = ""
-  protected def protectedMethod1: String = ""
-  def publicMethod1: String = ""
+  private def privateMethod1(): String = ""
+  protected def protectedMethod1(): String = ""
+  def publicMethod1(): String = ""
 
 
 class InheritedClass1 extends BaseClass1, Trait1, Trait2 :
@@ -119,8 +121,8 @@ class InheritedClass1 extends BaseClass1, Trait1, Trait2 :
   // /*override*/ def publicVarField1_= (v: String): Unit = ""
 
   private def privateMethod1: String = ""
-  protected override def protectedMethod1: String = ""
-  override def publicMethod1: String = ""
+  protected override def protectedMethod1(): String = ""
+  override def publicMethod1(): String = ""
 
   private val privateValField2: String = ""
   protected val protectedValField2: String = ""
@@ -130,9 +132,9 @@ class InheritedClass1 extends BaseClass1, Trait1, Trait2 :
   protected var protectedVarField2: String = ""
   var publicVarField2: String = ""
 
-  private def privateMethod2: String = ""
-  protected def protectedMethod2: String = ""
-  def publicMethod2: String = ""
+  private def privateMethod2(): String = ""
+  protected def protectedMethod2(): String = ""
+  def publicMethod2(): String = ""
 
 
 abstract class InheritedFromJavaClass1 extends BaseJavaClass2, Trait1, Trait2 :
