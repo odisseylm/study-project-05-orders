9abae75404b01359dafa9e4ab6d84a94de7552b4
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-02-15 19:39:17 +0200

    Scala ... => map-struct support module => enum mappers

---
+++ scala-map-struct-support/src/main/scala/org/mvv/scala/mapstruct/mappers/enumMappers.scala
@@ -0,0 +1,139 @@
+package org.mvv.scala.mapstruct.mappers
+
+import scala.quoted.{Expr, Quotes, Type}
+//
+import org.mvv.scala.mapstruct.{ Logger }
+
+
+private val log: Logger = Logger("org.mvv.scala.mapstruct.mappers.enumMappers")
+
+
+inline def enumMappingFunc[EnumFrom /*<: scala.reflect.Enum*/, EnumTo /*<: scala.reflect.Enum*/]
+  (): (EnumFrom => EnumTo) = ${ enumMappingFuncImpl[EnumFrom, EnumTo]() }
+
+
+def enumMappingFuncImpl[EnumFrom /*<: scala.reflect.Enum*/, EnumTo /*<: scala.reflect.Enum*/]
+  ()
+  (using quotes: Quotes)(using etFrom: Type[EnumFrom])(using etTo: Type[EnumTo]):
+    Expr[(EnumFrom => EnumTo)] =
+
+  import quotes.reflect.*
+
+  //val enumFromFromTypeRepr: TypeRepr = TypeRepr.of[EnumFrom]
+  //val enumToFromTypeRepr: TypeRepr = TypeRepr.of[EnumTo]
+
+  val enumFromClassName: String = Type.show[EnumFrom]
+  val enumToClassName: String = Type.show[EnumTo]
+
+  val logPrefix = s"enumMappingFuncImpl [ $enumFromClassName => $enumToClassName ], "
+
+
+  def enumValues[EnumType /*<: scala.reflect.Enum*/](using Type[EnumType]): List[String] =
+    val classSymbol: Symbol = Symbol.classSymbol(Type.show[EnumType]) // Symbol.requiredClass(typeNameStr)
+    val children: List[Symbol] = classSymbol.children
+    // maybe with complex enums we need to filter out non-enum items
+    val enumNames: List[String] = children.map(_.name)
+    enumNames
+
+  val useFullEnumClassName = true
+  def enumValue[EnumType](using Type[EnumType])(enumValue: String): Term =
+    if useFullEnumClassName
+      then enumValueUsingFullClassName[EnumType](enumValue)
+      else enumValueUsingSimpleClassNameAndEnumClassThisScope[EnumType](enumValue)
+
+  val enumFromValues = enumValues[EnumFrom]
+  val enumToValues = enumValues[EnumTo]
+  val allEnumValues = (enumFromValues ++ enumToValues).distinct
+
+  log.trace(s"$logPrefix => enumFromValues: $enumFromValues")
+  log.trace(s"$logPrefix => enumToValues  : $enumToValues")
+  log.trace(s"$logPrefix => allEnumValues : $allEnumValues")
+
+  def unexpectedEnumValuesErrorMsg(enumClassName: String, unexpectedEnumValues: IterableOnce[String]): Option[String] =
+    val unexpectedEnumValuesIt = unexpectedEnumValues.iterator
+    if unexpectedEnumValuesIt.nonEmpty
+      then Option(s"Enum [$enumClassName] has non-mapped values ${unexpectedEnumValuesIt.mkString("[", ", ", "]")}")
+      else None
+
+  val unexpectedEnumFromValues: List[String] = (enumFromValues diff enumToValues)
+  val unexpectedEnumToValues: List[String] = (enumToValues diff enumFromValues)
+
+  val err1: Option[String] = unexpectedEnumValuesErrorMsg(enumFromClassName, unexpectedEnumFromValues)
+  val err2: Option[String] = unexpectedEnumValuesErrorMsg(enumToClassName, unexpectedEnumToValues)
+  val allErrors = List(err1, err2).filter(_.isDefined).map(_.get).mkString(", ")
+
+  if allErrors.nonEmpty then
+    // ?? or better to use error() an return something??
+    report.errorAndAbort(allErrors)
+
+  // if enums are not symmetric it will cause compilation error of generated (by this macro) scala code
+  // what is expected/desired behavior
+
+  val rhsFn: (Symbol, List[Tree]) => Tree = (s: Symbol, paramsAsTrees: List[Tree]) => {
+    log.trace(s"$logPrefix rhsFn { s: $s, $paramsAsTrees }")
+
+    val caseDefs: List[CaseDef] = allEnumValues.map(enumValueLabel =>
+      val selectFrom = enumValue[EnumFrom](enumValueLabel)
+      log.trace(s"$logPrefix selectFrom: $selectFrom")
+
+      val selectTo = enumValue[EnumTo](enumValueLabel)
+      log.trace(s"$logPrefix selectFrom: $selectTo")
+
+      val caseDef = CaseDef(
+        selectFrom,
+        None,
+        Block(
+          Nil,
+          selectTo,
+        )
+      )
+      log.trace(s"$logPrefix caseDef: $caseDef")
+      caseDef
+    )
+
+    val matchExpr: Match = Match(paramsAsTrees.head.asInstanceOf[Term], caseDefs)
+    log.trace(s"$logPrefix matchExpr: $matchExpr")
+
+    matchExpr
+  }
+
+
+  val anonFunLambda = Lambda(
+    Symbol.spliceOwner,
+    MethodType(
+      List("enumFromValue")) ( // parameter names
+      _ => List(TypeRepr.of[EnumFrom]),
+      _ => TypeRepr.of[EnumTo],
+    ),
+    rhsFn
+  )
+
+  println(s"anonFunLambda expr: ${anonFunLambda.asExprOf[(EnumFrom => EnumTo)].show}")
+
+  val inlined = Inlined(None, Nil, anonFunLambda)
+  val inlinedExpr = inlined.asExprOf[EnumFrom => EnumTo]
+  println(s"${inlinedExpr.show}")
+  inlinedExpr
+
+
+def enumValueUsingSimpleClassNameAndEnumClassThisScope[T](using quotes: Quotes)(using enumType: Type[T])(enumValueName: String): quotes.reflect.Term =
+  ???
+
+def enumValueUsingFullClassName[T](using quotes: Quotes)(using enumType: Type[T])(enumValueName: String): quotes.reflect.Term =
+  import quotes.reflect.*
+  val fullClassName: String = TypeRepr.of[T].widen.show
+
+  val parts: List[String] = fullClassName.split('.').toList
+  require(parts.nonEmpty, s"Invalid enum class [$fullClassName].")
+
+  if parts.sizeIs == 1 then
+    // TODO: to test it
+    val classIdent = Ident(Symbol.requiredClass(fullClassName).termRef)
+    return Select.unique(classIdent, enumValueName)
+
+  val rootPackageIdentCom = Ident(Symbol.requiredPackage(parts.head).termRef)
+  val resultingFullClassNameSelect = parts.tail.tail.foldLeft
+    (Select.unique(rootPackageIdentCom, parts.tail.head))
+    ((preSelect, nextPart) => Select.unique(preSelect, nextPart))
+  Select.unique(resultingFullClassNameSelect, enumValueName)
+
---
+++ scala-map-struct-support/src/test/scala/org/mvv/scala/mapstruct/mappers/EnumMappersTest.scala
@@ -0,0 +1,85 @@
+package org.mvv.scala.mapstruct.mappers
+
+import scala.language.unsafeNulls
+//
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.SoftAssertions
+
+
+enum TestEnum1 :
+  case TestEnumValue1, TestEnumValue2
+enum TestEnum2 :
+  case TestEnumValue1, TestEnumValue2
+
+enum TestEnum11 :
+  case TestEnumValue1, TestEnumValue2, TestEnumValue3
+enum TestEnum12 :
+  case TestEnumValue1, TestEnumValue2, TestEnumValue4
+
+enum TestEnum21 :
+  case TestEnumValue1, TestEnumValue2, TestEnumValue3
+enum TestEnum22 :
+  case TestEnumValue1, TestEnumValue2
+
+
+class EnumMappersTest {
+
+  @Test
+  def test1(): Unit = {
+
+    val mapFunc: (TestEnum1 => TestEnum2) = enumMappingFunc[TestEnum1, TestEnum2]()
+
+    val a = SoftAssertions()
+
+    a.assertThat(mapFunc(TestEnum1.TestEnumValue1)).isEqualTo(TestEnum2.TestEnumValue1)
+    a.assertThat(mapFunc(TestEnum1.TestEnumValue2)).isEqualTo(TestEnum2.TestEnumValue2)
+
+    a.assertAll()
+  }
+
+  @Test
+  def testLocalEnums(): Unit = {
+    import EnumMappersTest.LocalTestEnum1
+    import EnumMappersTest.LocalTestEnum2
+
+    val mapFunc: (EnumMappersTest.LocalTestEnum1 => LocalTestEnum2) = enumMappingFunc[LocalTestEnum1, LocalTestEnum2]()
+
+    val a = SoftAssertions()
+
+    a.assertThat(mapFunc(LocalTestEnum1.TestEnumValue1)).isEqualTo(LocalTestEnum2.TestEnumValue1)
+    a.assertThat(mapFunc(LocalTestEnum1.TestEnumValue2)).isEqualTo(LocalTestEnum2.TestEnumValue2)
+
+    a.assertAll()
+  }
+
+  // it causes compilation error as expected
+  // ?? How to test it? ??
+  /*
+  @Test
+  def testAsymmetricEnums(): Unit = {
+    val mapFunc: (TestEnum11 => TestEnum12) = enumMappingFunc[TestEnum11, TestEnum12]()
+
+    val a = SoftAssertions()
+
+    a.assertThat(mapFunc(TestEnum11.TestEnumValue1)).isEqualTo(TestEnum12.TestEnumValue1)
+    a.assertThat(mapFunc(TestEnum11.TestEnumValue2)).isEqualTo(TestEnum12.TestEnumValue1)
+
+    a.assertAll()
+  }
+  */
+}
+
+def usingLocalClasses(): Unit = {
+  val enumValue1 = EnumMappersTest.LocalTestEnum1.TestEnumValue1
+  println(s"enumValue1: $enumValue1")
+}
+
+
+// "static" classes should be placed inside the companion object
+object EnumMappersTest :
+
+  enum LocalTestEnum1 :
+    case TestEnumValue1, TestEnumValue2
+
+  enum LocalTestEnum2 :
+    case TestEnumValue1, TestEnumValue2
