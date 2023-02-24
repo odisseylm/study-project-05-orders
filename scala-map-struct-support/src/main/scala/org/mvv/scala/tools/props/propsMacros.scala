59c3de50eff8e9afd97b501a3bde635f272051fe
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-02-24 17:48:54 +0200

    Scala ... => macros => NamedValue macros (simplified version)

--- scala-map-struct-support/src/main/scala/org/mvv/scala/tools/props/propsMacros.scala
+++ scala-map-struct-support/src/main/scala/org/mvv/scala/tools/props/propsMacros.scala
@@ -6,6 +6,25 @@ import org.mvv.scala.tools.quotes.qClassNameOf
 
 
 inline def namedValue[T](inline valueWithName: T): NamedValue[T] =
+  NamedValue[T](valueExprName(valueWithName), valueWithName)
+
+
+inline def valueExprName[T](inline valueWithName: T): String =
+  ${ valueExprNameImpl[T]('valueWithName) }
+
+
+def valueExprNameImpl[T]
+  (using q: Quotes)(using Type[T])
+  (valueWithName: Expr[T]): Expr[String] =
+
+  import q.reflect.{ Inlined, Literal, StringConstant }
+  val nameOfValueWithName = valueWithName.show
+  val resInlined = Inlined(None, Nil, Literal(StringConstant(nameOfValueWithName)))
+  resInlined.asExprOf[String]
+
+
+/*
+inline def namedValue[T](inline valueWithName: T): NamedValue[T] =
   ${ namedValueImpl[T]('valueWithName) }
 
 
@@ -22,3 +41,4 @@ def namedValueImpl[T]
   val namedValueApply = Apply(typeApply, List(Literal(StringConstant(nameOfValueWithName)), valueWithName.asTerm))
   val resInlined = Inlined(None, Nil, namedValueApply)
   resInlined.asExprOf[NamedValue[T]]
+*/
